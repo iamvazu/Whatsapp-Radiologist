ac6643a788b89c9503d2666536b0f00519d4fd71
S K Aravind <aravindraghvi@gmail.com>
2018-07-05 11:42:00 +0530

    Bot.py

--- loreal.py
+++ loreal.py
@@ -4,14 +4,12 @@ import os
 import logging
 
 def caption(image_path):
-        # Replace <Subscription Key> with your valid subscription key.
-    subscription_key = "Your API Key"
+    # Replace <Subscription Key> with your valid subscription key.
+    subscription_key = ""
     assert subscription_key
     vision_base_url = "https://eastus.api.cognitive.microsoft.com/vision/v2.0/"
 
     analyze_url = vision_base_url + "analyze"
-    # Set image_path to the local path of an image that you want to analyze.
-    # Read the image into a byte array
     image_data = open(image_path, "rb").read()
     headers    = {'Ocp-Apim-Subscription-Key': subscription_key,
                   'Content-Type': 'application/octet-stream'}
